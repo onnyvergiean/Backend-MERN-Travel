328a155c9a2a22c2410c2118867fa13ebe3d2ade
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2021-12-12 16:43:33 +0700

    Add error handling and alert message

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const adminRouter = require("./routes/admin");
 const methodOverride = require("method-override");
-
+const session = require("express-session");
+const flash = require("connect-flash");
 var app = express();
 
 // view engine setup
@@ -19,6 +20,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
