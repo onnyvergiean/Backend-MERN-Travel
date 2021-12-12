7fed82020c3d72d6d2977375ac5f8edab948091d
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2021-12-12 15:35:54 +0700

    Add method delete on Category

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -20,6 +20,12 @@ module.exports = {
     await category.save();
     res.redirect("/admin/category");
   },
+  deleteCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findOne({ _id: id });
+    await category.remove();
+    res.redirect("/admin/category");
+  },
   viewBank: (req, res) => {
     res.render("admin/bank/view_bank");
   },
