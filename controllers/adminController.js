6ec3c15bf312be079508c03685369f77381fe482
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2021-12-29 20:22:37 +0700

    Add dashboard info

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -59,11 +59,17 @@ module.exports = {
     req.session.destroy();
     res.redirect("/admin/signin");
   },
-  viewDashboard: (req, res) => {
+  viewDashboard: async (req, res) => {
     try {
+      const member = await Member.find();
+      const booking = await Booking.find();
+      const item = await Item.find();
       res.render("admin/dashboard/view_dashboard", {
         title: "Staycation | Dashboard",
         user: req.session.user,
+        member,
+        booking,
+        item,
       });
     } catch (error) {}
   },
